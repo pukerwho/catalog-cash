ab494458ec68ac0b8108f2a679239f0e3700516a
Puker Who <nawwesele@gmail.com>
2024-05-28 14:50:00 +0300

    edit

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -67,6 +67,19 @@
               </div>
             </div>
           </li>
+          <li class="menu-item relative hover:bg-theme-yellow rounded-lg mb-1">
+            <a href="<?php echo get_page_url("page-bookmarks"); ?>" class="absolute-link"></a>
+            <div class="flex items-center p-2">
+              <div class="mr-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
+              </div>
+              <div>
+                <?php _e("Закладки", "treba-wp"); ?>
+              </div>
+            </div>
+          </li>
           <li class="relative hover:bg-theme-yellow rounded-lg mb-1">
             <a href="/contacts" class="absolute-link"></a>
             <div class="flex items-center p-2">
